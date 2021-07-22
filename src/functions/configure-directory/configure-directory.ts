a84d39d7dd268a341efcdc0c279364e7ee0dfe87
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-07-21 22:37:16 -0400

    configure directory

--- src/functions/configure-directory/configure-directory.ts
+++ src/functions/configure-directory/configure-directory.ts
@@ -1,11 +1,27 @@
+import { Config } from "../../types/config";
+import { promises as fileSystem } from "fs";
+import { isDirectory } from "path-type";
+import promptForConfigValues from "../prompt-for-config-values";
+
 /**
  * Configure a directory for storytoy.
  *
  * @param {string} directory The directory to configure for storytoy.
  */
 export const configureDirectory = async (directory = ".") => {
-  // is directory a directory?
-  // prompt for .storytoy file values
-  // create .storytoy file in directory
-  console.log(`configure directory ${directory}`);
+  try {
+    if (!(await isDirectory(directory))) {
+      throw new Error(`${directory} is not a directory.`);
+    }
+    promptForConfigValues().then((config: Config) => {
+      fileSystem.appendFile(
+        `${directory}/storytoy.json`,
+        JSON.stringify(config, null, 2)
+      );
+    });
+  } catch (error) {
+    throw new Error(
+      `Unable to configure ${directory} for storytoy; ${error.message}`
+    );
+  }
 };
