786c1e09a195b96940d9a4fa175b7a18c6bc4d0d
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-07-24 22:47:22 -0400

    create a new choice

--- src/functions/create-choice/create-choice.ts
+++ src/functions/create-choice/create-choice.ts
@@ -1,11 +1,32 @@
+import isUniqueDirectoryName from "../is-unique-directory-name";
+import loadConfig from "../load-config";
+import { writeChoiceDataJson } from "../write-choice-data-json/write-choice-data-json";
+import { writeIndexJs } from "../write-index-js/write-index-js";
+import writeOnRenderJs from "../write-on-render-js";
+import writeReadmeMd from "../write-readme-md";
+
 /**
  * Create a new choice.
  *
- * @param {string} directory The path for the new choice.
+ * @param {string} id The id of the new choice.
+ * @returns {void}
  */
-export const createChoice = (directory: string) => {
-  // is directory a directory?
-  // is id unique in the given directory?
-  // copy and populate template choice directory into target directory
-  console.log(`create choice in ${directory}`);
+export const createChoice = (id: string) => {
+  return loadConfig()
+    .then(async (config) => {
+      const choicesDirectoryPath = config.choices;
+      if (!(await isUniqueDirectoryName(id, choicesDirectoryPath))) {
+        throw new Error(
+          `${choicesDirectoryPath} already contains a directory named ${id}`
+        );
+      }
+      const newChoiceDirectoryPath = `${choicesDirectoryPath}/${id}`;
+      writeChoiceDataJson(newChoiceDirectoryPath, id);
+      writeIndexJs(newChoiceDirectoryPath);
+      writeOnRenderJs(newChoiceDirectoryPath);
+      writeReadmeMd(newChoiceDirectoryPath, id);
+    })
+    .catch((error) => {
+      throw new Error(`Unable to create choice; ${error.message}`);
+    });
 };
