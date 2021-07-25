d7db27da00d67a385f078858e0763c15867302e9
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-07-25 13:24:20 -0400

    document writeChoiceDataJson function

--- src/functions/write-choice-data-json/write-choice-data-json.ts
+++ src/functions/write-choice-data-json/write-choice-data-json.ts
@@ -1,8 +1,17 @@
 import { promises as fileSystem } from "fs";
 
+/**
+ * Write boilerplate for choice-data.json to the given directory.
+ *
+ * @param {string} directory The parent directory for the new file.
+ * @param {string} id The id of the new Choice.
+ * @returns {Promise<void>} An empty promise.
+ *
+ * @todo {MUST} test
+ */
 export const writeChoiceDataJson = (directory: string, id: string) => {
   return fileSystem.mkdir(directory).then(() => {
-    fileSystem.writeFile(
+    return fileSystem.writeFile(
       `${directory}/choice-data.json`,
       `{
 "axes": [],
