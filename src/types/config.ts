2cd5534ce91904e141f95cf1eee0ba3d208d2353
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-07-24 21:37:15 -0400

    mark choices field as required

--- src/types/config.ts
+++ src/types/config.ts
@@ -1,6 +1,11 @@
+/**
+ * @todo document
+ * @todo import IConfig from storytoy
+ * @todo explicitly implement IConfig
+ */
 export type Config = {
   author?: string;
-  choices?: string;
+  choices: string;
   description?: string;
   title?: string;
 };
