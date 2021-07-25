e2f3ad31e644731e790baa40fb85ee52421aec11
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-07-25 13:23:54 -0400

    document writeIndexJs function

--- src/functions/write-index-js/write-index-js.ts
+++ src/functions/write-index-js/write-index-js.ts
@@ -1,5 +1,14 @@
 import { promises as fileSystem } from "fs";
 
+/**
+ * Write boilerplate for index.js to the given directory.
+ *
+ * @param {string} directory The parent directory for the new file.
+ * @returns {Promise<void>} An empty promise.
+ *
+ * @todo {MUST} test
+ * @todo {SHOULD} include comment with file
+ */
 export const writeIndexJs = (directory: string) => {
   return fileSystem.mkdir(directory).then(() => {
     fileSystem.writeFile(
