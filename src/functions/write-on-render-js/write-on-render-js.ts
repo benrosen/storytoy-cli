8a06c7ce63f9ec8ce43a03eb5a1588a66f359912
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-07-25 13:22:21 -0400

    document writeOnRender function

--- src/functions/write-on-render-js/write-on-render-js.ts
+++ src/functions/write-on-render-js/write-on-render-js.ts
@@ -1,5 +1,13 @@
 import { promises as fileSystem } from "fs";
 
+/**
+ * Write boilerplate for on-render.js to the given directory.
+ *
+ * @param {string} directory The parent directory for the new file.
+ * @returns {Promise<void>} An empty promise.
+ *
+ * @todo {MUST} test
+ */
 export const writeOnRenderJs = (directory: string) => {
   return fileSystem.mkdir(directory).then(() => {
     fileSystem.writeFile(
