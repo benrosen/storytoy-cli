63371f805d337c4efdf4d6f46d6f04467035f6db
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-07-25 13:21:58 -0400

    document writeReadmeMd function

--- src/functions/write-readme-md/write-readme-md.ts
+++ src/functions/write-readme-md/write-readme-md.ts
@@ -1,5 +1,15 @@
 import { promises as fileSystem } from "fs";
 
+/**
+ * Write boilerplate for README.md to the given directory.
+ *
+ * @param {string} directory The parent directory for the new file.
+ * @param {string} id The id of the new Choice.
+ * @returns {Promise<void>} An empty promise.
+ *
+ * @todo {SHOULD} add content
+ * @todo {MUST} test
+ */
 export const writeReadmeMd = (directory: string, id: string) => {
-  fileSystem.writeFile(`${directory}/README.md`, `# ${id}`);
+  return fileSystem.writeFile(`${directory}/README.md`, `# ${id}`);
 };
