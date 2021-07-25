865bd01e33fd53607009325233ffc926a1873f8b
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-07-24 21:41:00 -0400

    refactor to use writeJsonFile function

--- src/functions/configure-directory/configure-directory.ts
+++ src/functions/configure-directory/configure-directory.ts
@@ -1,12 +1,19 @@
 import { Config } from "../../types/config";
-import { promises as fileSystem } from "fs";
 import { isDirectory } from "path-type";
 import promptForConfigValues from "../prompt-for-config-values";
+import writeJsonFile from "../write-json-file";
 
 /**
  * Configure a directory for storytoy.
  *
  * @param {string} directory The directory to configure for storytoy.
+ *
+ * @todo document errors
+ * @todo test
+ * @todo support flag that enables user to accept all default values and skip prompt
+ * @todo create a "choices" directory using config.choices path
+ * @todo support variable number of indentation spaces for config file
+ * @todo include stack trace in thrown errors
  */
 export const configureDirectory = async (directory = ".") => {
   try {
@@ -14,10 +21,7 @@ export const configureDirectory = async (directory = ".") => {
       throw new Error(`${directory} is not a directory.`);
     }
     promptForConfigValues().then((config: Config) => {
-      fileSystem.appendFile(
-        `${directory}/storytoy.json`,
-        JSON.stringify(config, null, 2)
-      );
+      writeJsonFile(config, `${directory}/storytoy.json`);
     });
   } catch (error) {
     throw new Error(
