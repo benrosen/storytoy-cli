8caa6dd486fa87081a72de4e8fff31ac0392ce26
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-07-26 16:52:21 -0400

    ensure that choices directory specified in storytoy.config exists

--- src/functions/configure-directory/configure-directory.ts
+++ src/functions/configure-directory/configure-directory.ts
@@ -1,14 +1,15 @@
 import { Config } from "../../types/config";
+import { ensureDir } from "fs-extra";
 import { isDirectory } from "path-type";
 import promptForConfigValues from "../prompt-for-config-values";
 import writeJsonFile from "../write-json-file";
+const path = require("path");
 
 /**
  * Configure a directory for storytoy.
  *
  * @param {string} directory The directory to configure for storytoy.
  *
- * @todo {MUST} create a "choices" directory using config.choices path
  * @todo {SHOULD} include stack trace in thrown errors
  * @todo {COULD} support flag that enables user to accept all default values and skip prompt
  * @todo {COULD} support variable number of indentation spaces for config file
@@ -19,6 +20,7 @@ export const configureDirectory = async (directory = ".") => {
       throw new Error(`${directory} is not a directory.`);
     }
     promptForConfigValues().then((config: Config) => {
+      ensureDir(path.join(directory, config.choices));
       writeJsonFile(config, `${directory}/storytoy.json`);
     });
   } catch (error) {
