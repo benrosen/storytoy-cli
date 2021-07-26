a1459eb20dfddd3631363eee33d2bf9a0cd44701
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-07-26 17:58:49 -0400

    update storytoy to version that exports types

--- src/types/choice-data.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @todo {MUST} document
- * @todo {MUST} import Choice from storytoy
- * @todo {SHOULD} replace instances of Choice with ChoiceData where appropriate
- */
-export type ChoiceData = {}; // Omit<Choice, "onRender">;
